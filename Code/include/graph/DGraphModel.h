b3ceb8274cfa407ae33a4d2930ab704468e7ffb3
ADMIN\Administrator <tien.nguyen20020713@gmail.com>
2024-12-14 00:11:40 +0700

    Complete task DGraphModel but not checked for testcases

--- Code/include/graph/DGraphModel.h
+++ Code/include/graph/DGraphModel.h
@@ -16,8 +16,8 @@
 #include "graph/AbstractGraph.h"
 #include "stacknqueue/Queue.h"
 #include "stacknqueue/Stack.h"
-#include "hash/XHashMap.h"
-#include "stacknqueue/PriorityQueue.h"
+#include "hash/xMap.h"
+// #include "stacknqueue/PriorityQueue.h"
 #include "sorting/DLinkedListSE.h"
 
 
@@ -36,21 +36,89 @@ public:
         AbstractGraph<T>(vertexEQ, vertex2str){
     }
     
-    void connect(T from, T to, float weight=0){
+    void connect(T from, T to, float weight=0)
+    {
         //TODO
+        typename AbstractGraph<T>::VertexNode *fromNode = this -> getVertexNode(from);
+        typename AbstractGraph<T>::VertexNode *toNode = this -> getVertexNode(to);
+
+        if(!fromNode) throw VertexNotFoundException(this -> vertex2str(from));
+        if(!toNode) throw VertexNotFoundException(this -> vertex2str(to));
+
+        fromNode -> connect(toNode, weight);
     }
-    void disconnect(T from, T to){
+
+    void disconnect(T from, T to)
+    {
         //TODO
+        typename AbstractGraph<T>::VertexNode *fromNode = this -> getVertexNode(from);
+        typename AbstractGraph<T>::VertexNode *toNode = this -> getVertexNode(to);
+
+        if(!fromNode) throw VertexNotFoundException(this -> vertex2str(from));
+        if(!toNode) throw VertexNotFoundException(this -> vertex2str(to));
+
+        typename AbstractGraph<T>::Edge *edge = fromNode -> getEdge(toNode);
+
+        if(!edge)
+        {
+            typename AbstractGraph<T>::Edge solution(fromNode, toNode);
+            throw EdgeNotFoundException(this -> edge2Str(solution));
+        }
+
+        else fromNode -> removeTo(toNode);
     }
-    void remove(T vertex){
+
+    void remove(T vertex)
+    {
         //TODO
+        typename AbstractGraph<T>::VertexNode *fromNode = this -> getVertexNode(vertex);
+        if(fromNode == nullptr) throw VertexNotFoundException(this -> vertex2str(vertex));
+        DLinkedList<T> outEdge = this -> getOutwardEdges(vertex);
+
+        for(typename DLinkedList<T>::Iterator it = outEdge.begin(); it != outEdge.end(); it++)
+        {
+            typename AbstractGraph<T>::VertexNode *toNode = this -> getVertexNode(*it);
+
+            if(toNode != nullptr)
+            {
+                fromNode -> removeTo(toNode);
+            }
+        }
+
+        DLinkedList<T> inEdge = this -> getInwardEdges(vertex);
+        for (typename DLinkedList<T>::Iterator it = inEdge.begin(); it != inEdge.end(); it++)
+        {
+            typename AbstractGraph<T>::VertexNode *toNode = this->getVertexNode(*it);
+
+            if(toNode != nullptr)
+            {
+                toNode -> removeTo(fromNode);
+            }
+        }
+
+        this -> nodeList.removeItem(fromNode);
+        delete fromNode;
     }
     
     static DGraphModel<T>* create(
             T* vertices, int nvertices, Edge<T>* edges, int nedges,
             bool (*vertexEQ)(T&, T&),
-            string (*vertex2str)(T&)){
+            string (*vertex2str)(T&))
+    {
         //TODO
+        DGraphModel<T> *graph = new DGraphModel<T>(vertexEQ, vertex2str);
+
+        for(int i = 0; i < nvertices; i++)
+        {
+            graph -> add(vertices[i]);
+        }
+
+        for(int i = 0; i < nedges; i++)
+        {
+            graph -> connect(edges[i].from, edges[i].to, edges[i].weight);
+        }
+
+        return graph;
     }
 };
 
